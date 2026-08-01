ab79b7c10b57a6c64af6b725d7fb7a2911b6f70f
Elvin Ambasa <elvinambasa@gmail.com>
2026-08-01 15:39:34 +0300

    Add border to table tooltip content

--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -71,7 +71,7 @@ export function TableRow({
             )}
           </button>
         </TooltipTrigger>
-        <TooltipContent side="right" className="text-xs">
+        <TooltipContent side="right" className="text-xs border border-border">
           <div className="space-y-1">
             <div className="font-semibold">{table.name}</div>
             <div className="text-muted-foreground">
